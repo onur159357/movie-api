03d80da1240b79a36d7e662946ccafa034391389
onur159357 <onurrryilmaz@gmail.com>
2018-07-23 15:49:00 +0300

    category[GET], movielist[GET] group join mongodb

--- routes/category/category-get-id.js
+++ routes/category/category-get-id.js
@@ -7,7 +7,55 @@ const mongoose = require('mongoose'),
 router.get('/:category_id', (req, res, next) => {
     const categoryIdSrch = () => {
         return new Promise((resolve, reject) => {
-            const categorySchema = CategorySchema.findById({_id : req.params.category_id});
+            const categorySchema = CategorySchema.aggregate(
+            [
+                {
+                    $match : {_id : mongoose.Types.ObjectId(req.params.category_id)}
+                }, {
+                    $lookup : {
+                        from : 'movies', //hangi tablo(collection) ile join yapacağız.
+                        localField : '_id', // category tablomdaki hangi alanı eşleştireceğim.
+                        foreignField : 'category', // movies tablomdan karşılık gelecek alan
+                        as : 'movie', // bu değerlerin atanacağı değişken.
+                    }
+                },{
+                    $unwind : {
+                        path : '$movie',
+                        preserveNullAndEmptyArrays : true,
+                    },
+                },{
+                    $group : {
+                        _id : {
+                            _id : '$_id',
+                            category_name : '$category_name',
+                            category_sub_number : '$category_sub_number',
+                            category_description : '$category_description',
+                            category_id : '$category_id',
+                        }, 
+                        movie : {
+                            $push : '$movie',
+                            
+                            //ALTERNATİF
+                            // $push : {
+                            //     movie_name : '$movie.movie_name',
+                            //     imdb_score : '$movie.imdb_score'
+                            // }
+                        }
+                    }
+                }, {
+                    $project : {
+                        _id : '$_id._id',
+                        category_name : '$_id.category_name',
+                        category_sub_number : '$_id.category_sub_number',
+                        category_description : '$_id.category_description',
+                        category_id : '$_id.category_id',
+                        movie : '$movie',
+                        
+                    }
+                }
+            ]
+                
+            );
 
             categorySchema.then((data) => {
                 if(!data) {
--- routes/movie/movie-get-list.js
+++ routes/movie/movie-get-list.js
@@ -3,7 +3,49 @@ const express = require('express'),
 //MODEL
 const MovieSchema = require('../../model/Movies');
 router.get('/', (req, res, next) => {
-    const movieSchema = MovieSchema.find();
+    const movieSchema = MovieSchema.aggregate(
+        [ 
+            {
+                $lookup : {
+                    from : 'directors', //hangi tablo(collection) ile join yapacağız.
+                    localField : 'director', // movie tablomdaki hangi alanı eşleştireceğim.
+                    foreignField : '_id', // directors tablomdan karşılık gelecek alan
+                    as : 'directors', // bu değerlerin atanacağı değişken.
+                }
+            },
+            {
+                $unwind : '$directors', // yukarıda as ile tanımlanan değişken
+                // preserveNullAndEmptyArrays : True  
+            },
+            {
+                $lookup : {
+                    from : 'categories',
+                    localField : 'category',
+                    foreignField : '_id',
+                    as : 'category',
+                }
+            },
+            {
+                $unwind : '$category',
+            },
+            {
+                $project: {
+                    _id : 1,
+                    director : 1,
+                    category : 1,
+                    movie_name : 1,
+                    movie_country : 1,
+                    imdb_score : 1,
+                    movie_year : 1,
+                    movie_img : 1,
+                    movie_video : 1,
+                    category : '$category',
+                    //directors : '$directors',
+                    director_name : '$directors.director_name', // Sadece yazar ismini almak için kullanıyoruz
+                }
+            }
+        ]
+    );
     
     movieSchema.then((data) => {
         res.json(data);
