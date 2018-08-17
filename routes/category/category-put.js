d0aa1e611f0f693b8a7820ad0fb6c6fe3d60aa6d
onur159357 <onurrryilmaz@gmail.com>
2018-08-17 16:00:45 +0300

    category[PUT] unique controler add

--- routes/category/category-put.js
+++ routes/category/category-put.js
@@ -4,27 +4,87 @@ const express = require('express'),
 //MODEL
 const mongoose = require('mongoose'),
       CategorySchema = require('../../model/Category');
+//unique controler
 
 router.put('/:category_id', (req, res, next) => {
-    const categorySchema = CategorySchema.findByIdAndUpdate(
-        req.params.category_id,
-        req.body,
-        { new:true,}
-    );
 
-    categorySchema.then((data) => {
-        if(!data) {
-            res.json({err : 'böyle bir sayfa yok'})
+    //unique controler
+    const uniqueControler = () => {
+        return new Promise((resolve, reject) => {
+            let errMsg = {
+                'faultyArea' : [],
+                'result' : Boolean,
+        
+            };
+            const categorySchema = CategorySchema.find({category_name : req.body.category_name});
+            categorySchema.then((data) => {
+                if(data.length > 0) {
+                    errMsg.faultyArea = data[0].category_name;
+                    errMsg.result = false;
+                    resolve(errMsg);
+    
+                } else {
+                    errMsg.result = true; 
+    
+                }
+                resolve(errMsg);
+    
+            }).catch((err) => {
+                reject(err);
+    
+            });
+
+        })
+
+    };
+
+    //Edit Function
+    const editFunction = () => {
+        return new Promise((resolve, reject) => {
+            if(req.body.category_name.length !== 0) {
+                const categorySchema = CategorySchema.findByIdAndUpdate(
+                    req.params.category_id,
+                    req.body,
+                    { new:true,}
+                );
+            
+                categorySchema.then((data) => {
+                    if(!data) {
+                        resolve ({err : 'böyle bir sayfa yok'});
+            
+                    } else {
+                        resolve (data);
+            
+                    }
+            
+                }).catch((error) => {
+                    reject (error);
+            
+                })
+        
+            } else {
+                res.json({err : 'Boş Kategori giremezsiniz'});
+        
+            }
+
+        })
+
+    }
+    
+    uniqueControler().then((data) => {
+        if(data.result === true) {
+            editFunction().then((data) => {
+                res.json(data);
+
+            });
 
         } else {
-            res.json(data);
+            res.json({ err : `${data.faultyArea} Kategorisinden mevcut`})
 
         }
-
-    }).catch((error) => {
-        res.json(error);
-
-    })
+        
+     });
+    
 })
 
 module.exports = router;
